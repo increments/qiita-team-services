40b81be5f194eb91dab8f0568b5404a4a080ea7b
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-09-07 01:33:34 +0900

    Add Events.create

--- lib/qiita/team/services/events.rb
+++ lib/qiita/team/services/events.rb
@@ -7,6 +7,20 @@ module Qiita::Team::Services
       def event_names
         @event_names ||= []
       end
+
+      # @param event_name [Symbol]
+      # @param resource [Resources::Base]
+      # @param team [Resources:Team]
+      def create(event_name, resource, team)
+        event_class(event_name).new(resource, team)
+      end
+
+      private
+
+      # @param event_name [String]
+      def event_class(event_name)
+        "Qiita::Team::Services::Events::#{event_name.to_s.camelize}".constantize
+      end
     end
   end
 end
