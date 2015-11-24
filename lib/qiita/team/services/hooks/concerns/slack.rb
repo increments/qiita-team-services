f40a7ebe8a42c4f6c1032d4a3d5dce6c103bce8a
ganmacs <ganmacs@gmail.com>
2015-11-24 17:56:00 +0900

    replace comment string with comment url

--- lib/qiita/team/services/hooks/concerns/slack.rb
+++ lib/qiita/team/services/hooks/concerns/slack.rb
@@ -93,9 +93,9 @@ module Qiita::Team::Services
         def item_comment_created(event)
           fallback =
             if event.item.coediting?
-              "New comment on #{item_link(event.item)}"
+              "New #{comment_link(event.comment)} on #{item_link(event.item)}"
             else
-              "New comment on #{user_link(event.item.user)}'s #{item_link(event.item)}"
+              "New #{comment_link(event.comment)} on #{user_link(event.item.user)}'s #{item_link(event.item)}"
             end
           send_message(
             attachments: [
@@ -132,7 +132,7 @@ module Qiita::Team::Services
         # @return [void]
         # @raise [Qiita::Team::Services::DeliveryError]
         def project_comment_created(event)
-          fallback = "New comment on #{project_link(event.project)}"
+          fallback = "New #{comment_link(event.comment)} on #{project_link(event.project)}"
           send_message(
             attachments: [
               fallback: fallback,
