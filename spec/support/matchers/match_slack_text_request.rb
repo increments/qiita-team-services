1bb6dcdeb37386f63c6813f5bc8e713e52851097
Tomoya Chiba <tomo.asleep@gmail.com>
2015-12-03 17:39:08 +0900

    Check if slack hook escapes special characters
    Check if it works when
    - resource contains special characters
    - team contains special characters

--- spec/support/matchers/match_slack_text_request.rb
+++ spec/support/matchers/match_slack_text_request.rb
@@ -3,6 +3,11 @@ RSpec::Matchers.define :match_slack_text_request do
     request_body.is_a?(Hash) && \
       request_body.key?(:text) && \
       !request_body.key?(:attachments) && \
-      request_body[:text].is_a?(String)
+      request_body[:text].is_a?(String) && \
+      !illegal_slack_link_format?(request_body[:text])
+  end
+
+  def illegal_slack_link_format?(text)
+    text.match(/<[^>]+</)
   end
 end
