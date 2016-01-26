4b52cd20d6384781f499375d6b7ebd532e8148e5
Tomoya Chiba <tomo.asleep@gmail.com>
2016-01-26 17:38:28 +0900

    Truncate Slack message text to 7500 bytes

--- spec/support/matchers/match_slack_attachments_request.rb
+++ spec/support/matchers/match_slack_attachments_request.rb
@@ -2,6 +2,12 @@ RSpec::Matchers.define :match_slack_attachments_request do
   match do |request_body|
     request_body.is_a?(Hash) && \
       request_body.key?(:attachments) && \
-      request_body[:attachments].is_a?(Array)
+      request_body[:attachments].is_a?(Array) && \
+      request_body[:attachments].all?(&method(:text_valid_length?))
+  end
+
+  def text_valid_length?(attachment)
+    attachment[:text].nil? || \
+      attachment[:text].bytesize <= Qiita::Team::Services::Hooks::Concerns::Slack::TEXT_BYTESIZE_MAX
   end
 end
